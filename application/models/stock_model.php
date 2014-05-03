c7d08a043a40475a073ce5cfbbcaf1e59a7ec50b
Shamoon Siddiqui <EmailShamoon@gmail.com>
2014-05-03 12:04:13 -0400

    Fixed comma and quote delimiting issue in model

--- application/models/stock_model.php
+++ application/models/stock_model.php
@@ -5,12 +5,13 @@ class Stock_model extends CI_Model {
   public function get_stocks()
   {
     $stocks_csv = file_get_contents('http://finance.yahoo.com/d/quotes.csv?s=MSFT+GOOG+TSLA+MSFT+AAPL+GLD+MU&f=snl1');
+    $stocks_csv = substr($stocks_csv, 0, -1);
+
     $quote_array = explode("\n", $stocks_csv);
 
     $stocks = array();
     foreach ($quote_array as $quote) {
-      $quote = str_replace('"', '', $quote);
-      $stock_quote = explode(',', $quote);
+      $stock_quote = str_getcsv($quote, ',', '"');
       $stocks[] = $stock_quote;
     }
 

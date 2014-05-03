c92ab51183cd73c31af2c7e3f661832aac35ec8d
Shamoon Siddiqui <EmailShamoon@gmail.com>
2014-05-03 11:18:56 -0400

    Added a basic table

--- application/views/home.php
+++ application/views/home.php
@@ -1,1 +1,16 @@
-<h1>Hello, world!</h1>
+<div class="container">
+  <div class="row">
+    <h1>My Stocks</h1>
+    <div class="col-md-12">
+      <table class="table table-hover">
+        <thead>
+          <th>Symbol</th>
+          <th>Name</th>
+          <th>Last</th>
+          <th>High</th>
+          <th>Low</th>
+        </thead>
+      </table>
+    </div>
+  </div>
+</div>
